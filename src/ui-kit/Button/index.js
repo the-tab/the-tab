1e8e36a9814455912d8d95024e9f10d0f2cfe7f7
Ilya Atamas <atamas1llya@gmail.com>
2018-01-18 22:08:18 +0200

    Extend UI-kit

--- src/ui-kit/Button/index.js
+++ src/ui-kit/Button/index.js
@@ -5,17 +5,17 @@ import styles from './styles';
 const Button = ({ children, ...props }) => {
   const className = `
     button
-    ${props.primary && "primary"}
-    ${props.link && "link"}
-    ${props.danger && "danger"}
-  `
+    ${props.primary && 'primary'}
+    ${props.link && 'link'}
+    ${props.danger && 'danger'}
+  `;
   return (
     <button className={className}>
       <span>
         { children }
       </span>
     </button>
-  )
-}
+  );
+};
 
 export default Button;
