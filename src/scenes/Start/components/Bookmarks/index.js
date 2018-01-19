0d1b043447566cabac1fa800477928f47ec356e3
Ilya Atamas <atamas1llya@gmail.com>
2018-01-20 00:22:31 +0200

    Add bookmarks layout

--- src/scenes/Start/components/Bookmarks/index.js
+++ src/scenes/Start/components/Bookmarks/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './style.less';
+import Tale from '../Tale';
 
 export default class Bookmarks extends Component {
   render() {
@@ -8,7 +9,111 @@ export default class Bookmarks extends Component {
       <div
         id="bookmarks_dashboard"
         style={{ top: window.innerHeight - this.props.bottom }}
-      />
+      >
+        <section>
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua.
+            "
+          />
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet sit amet sit amet sit amet.
+            "
+          />
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua.
+            "
+          />
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet sit amet sit amet sit amet.
+            "
+          />
+        </section>
+        <section>
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet.
+              consectetur adipisicing elit.
+            "
+          />
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua.
+              Labore et dolore magna aliqua.
+            "
+          />
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet,
+              consectetur adipisicing enit ark,
+              ipsum dolor sit amet uni at.
+            "
+          />
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua.
+              Labore et dolore magna aliqua.
+            "
+          />
+        </section>
+        <section>
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua.
+            "
+          />
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet,
+              consectetur adipisicing enit ark.
+            "
+          />
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua.
+            "
+          />
+          <Tale
+            title="Bookmark"
+            description="
+              Lorem ipsum dolor sit amet,
+              consectetur adipisicing enit ark.
+            "
+          />
+        </section>
+      </div>
     );
   }
 }
---
+++ src/scenes/Start/components/Tale/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './style.less';
+
+const Tale = ({ title, description, image }) => (
+  <div className="tale">
+    <section className="header">
+      <div className="avatar">{ title && title.charAt(0) }</div>
+      <span>{ title }</span>
+    </section>
+    <section className="description">
+      { description }
+    </section>
+    <section className="image">
+
+    </section>
+  </div>
+)
+
+export default Tale;
