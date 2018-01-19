19bcfd52836c246274302002ab7edac2caa3cb0e
Ilya Atamas <atamas1llya@gmail.com>
2018-01-19 21:38:17 +0200

    Maaagic

--- src/scenes/Start/index.js
+++ src/scenes/Start/index.js
@@ -16,14 +16,21 @@ export default class StartScene extends Component {
     document.documentElement.removeEventListener('wheel', this.handleScroll);
   }
 
-  appearDistance = 150;
+  appearDistance = 180;
 
   updateElementTransition = (transition) => {
     const element = document.querySelector('#bookmarks_dashboard');
 
-    if (element) {
-      if (element.style.transition !== transition) {
-        element.style.transition = transition;
+    if (!this.transitionTimeFreezed) {
+      if (element) {
+        if (element.style.transition !== transition) {
+          element.style.transition = transition;
+
+          this.transitionTimeFreezed = true;
+          setTimeout(() => {
+            this.transitionTimeFreezed = false;
+          }, 1000 * parseInt(transition, 10));
+        }
       }
     }
   }
@@ -32,26 +39,42 @@ export default class StartScene extends Component {
     const element = document.querySelector('#bookmarks_dashboard');
     const nextPosition = this.state.bottom + (deltaY / 3);
 
-    if (element) {
-      if (this.state.bottom > this.appearDistance / 2 && nextPosition <= this.appearDistance / 2) {
-        this.updateElementTransition('0.5s');
-        this.setState({
-          bottom: (nextPosition - this.appearDistance) - (nextPosition - this.appearDistance),
-        });
-      } else if (nextPosition >= 0) {
-        if (this.state.bottom <= 0) {
-          this.updateElementTransition('0.5s');
-          this.transitionTimeFreezed = true;
-          setTimeout(() => {
-            this.transitionTimeFreezed = false;
-          }, 500);
-          this.setState({ bottom: nextPosition + this.appearDistance });
-        } else if (!this.transitionTimeFreezed) {
-          this.updateElementTransition('0s');
-          this.setState({ bottom: nextPosition });
-        }
-      }
+    if (this.state.bottom > this.appearDistance && nextPosition <= this.appearDistance) {
+      console.log('1');
+      this.updateElementTransition('0.5s');
+      this.setState({
+        bottom: (nextPosition - this.appearDistance) - (nextPosition - this.appearDistance),
+      });
+    } else if (this.state.bottom <= 0 && nextPosition >= 0) {
+      console.log('2');
+      this.updateElementTransition('0.5s');
+      this.setState({ bottom: nextPosition + this.appearDistance });
+    } else if (nextPosition >= 0) {
+      console.log('3');
+      this.updateElementTransition('0.1s');
+      this.setState({ bottom: nextPosition });
     }
+
+    // if (element) {
+    //   if (this.state.bottom > this.appearDistance / 2 && nextPosition <= this.appearDistance / 2) {
+    //     this.updateElementTransition('0.5s');
+    //     this.setState({
+    //       bottom: (nextPosition - this.appearDistance) - (nextPosition - this.appearDistance),
+    //     });
+    //   } else if (nextPosition >= 0) {
+    //     if (this.state.bottom <= 0) {
+    //       this.updateElementTransition('0.5s');
+    //       this.transitionTimeFreezed = true;
+    //       setTimeout(() => {
+    //         this.transitionTimeFreezed = false;
+    //       }, 500);
+    //       this.setState({ bottom: nextPosition + this.appearDistance });
+    //     } else if (!this.transitionTimeFreezed) {
+    //       this.updateElementTransition('0s');
+    //       this.setState({ bottom: nextPosition });
+    //     }
+    //   }
+    // }
   }
 
   render() {
