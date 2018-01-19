ad2468409ccb4d4302c0cbb1b9c41289036e4883
Ilya Atamas <atamas1llya@gmail.com>
2018-01-19 17:03:54 +0200

    Add scroll pane

--- src/scenes/Start/index.js
+++ src/scenes/Start/index.js
@@ -4,10 +4,31 @@ import './style.less';
 import Bookmarks from './components/Bookmarks';
 
 export default class StartScene extends Component {
+  state = {
+    bottom: 0,
+  }
+
+  componentDidMount() {
+    document.documentElement.addEventListener('wheel', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    document.documentElement.removeEventListener('wheel', this.handleScroll);
+  }
+
+  handleScroll = ({ movementY }) => {
+    this.setState(({ bottom }) => ({
+      bottom: bottom + movementY * 3,
+    }))
+  }
+
   render() {
     return (
       <div id="start_scene">
-        <Bookmarks />
+        <div id="start_scene_main_panel">
+          <h2>Scroll down</h2>
+        </div>
+        <Bookmarks bottom={this.state.bottom} />
       </div>
     );
   }
