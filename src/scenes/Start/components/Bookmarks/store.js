375017e301c26de05f5d82e974e2ac1977782d07
Atamas Ilya <atamas1llya@gmail.com>
2018-01-20 23:06:20 +0200

    Add bookmarks meta fetching

--- src/scenes/Start/components/Bookmarks/store.js
+++ src/scenes/Start/components/Bookmarks/store.js
@@ -1,38 +1,50 @@
 import { observable, action } from 'mobx';
 
+import { getPageInfo } from '../../../../utils/meta';
+
 class BookmarksStore {
   @observable bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  rawBookmarks = JSON.parse(localStorage.getItem('raw_bookmarks'));
   processedGraphs = {};
 
   @action setBookmarks = async (bookmarks) => {
-    this.bookmarks = this.processBookmarksTree(bookmarks);
+    if (JSON.stringify(bookmarks) == JSON.stringify(this.rawBookmarks)) {
+      this.rawBookmarks = bookmarks;
+      this.bookmarks = await this.processBookmarksTree(bookmarks);
 
-    console.log(this.bookmarks);
-    this.cacheBookmarks();
+      console.log(this.bookmarks);
+      this.cacheBookmarks();
+    }
   }
 
-  processBookmarksTree = tree => tree.map((b) => {
-    if (b.children) {
-      return {
-        folder: true,
-        title: b.title,
-        id: b.id,
-        children: this.processBookmarksTree(b.children),
+  processBookmarksTree = async (tree) => {
+    const promises = [];
+    
+    await tree.forEach((b) => {
+      if (b.children) {
+        promises.push(this.processBookmarksTree(b.children).then(children => ({
+          folder: true,
+          title: b.title,
+          id: b.id,
+          children,
+        })))
+      } else {
+        promises.push(getPageInfo(b.url).then(meta => ({
+          folder: false,
+          title: b.title,
+          id: b.id,
+          url: b.url,
+          meta,
+        })))
       }
-    } else {
-      return {
-        folder: false,
-        title: b.title,
-        id: b.id,
-        url: b.url,
-      }
-    }
+    })
 
-    return b;
-  })
+    return await Promise.all(promises);
+  };
 
   cacheBookmarks = () => {
     localStorage.setItem('bookmarks', JSON.stringify(this.bookmarks));
+    localStorage.setItem('raw_bookmarks', JSON.stringify(this.rawBookmarks));
   }
 }
 
