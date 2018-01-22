589c9e6867e8f1c5e5e285e878dc40b0b368be98
Atamas Ilya <atamas1llya@gmail.com>
2018-01-22 16:39:13 +0200

    Connect first module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Packager from './core/Packager';
 import Renderer from './core/Renderer';
 
-import testModule from './modules/test';
+import testModule from '../../the-tab-module-bookmarks';
 
 Packager.registerPackage(testModule);
 Renderer.render(document.querySelector('#root'));
